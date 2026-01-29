2cd4c6999d58caeea7ec70356d52d3f0156af1cd
Pedro Ruivo <pruivo@users.noreply.github.com>
2026-01-29 10:38:54 +0000

    Refactor SessionsResource for better memory usage and performance
    Closes #45727
    
    Signed-off-by: Pedro Ruivo <1492066+pruivo@users.noreply.github.com>
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>
    Co-authored-by: Pedro Ruivo <1492066+pruivo@users.noreply.github.com>
    Co-authored-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- model/infinispan/src/main/java/org/keycloak/models/sessions/infinispan/query/UserSessionQueries.java
+++ model/infinispan/src/main/java/org/keycloak/models/sessions/infinispan/query/UserSessionQueries.java
@@ -36,10 +36,10 @@ public final class UserSessionQueries {
     private static final String BY_BROKER_SESSION_ID = BASE_QUERY + "WHERE e.realmId = :realmId && e.brokerSessionId = :brokerSessionId ORDER BY e.userSessionId";
     private static final String BY_USER_ID = BASE_QUERY + "WHERE e.realmId = :realmId && e.userId = :userId ORDER BY e.userSessionId";
     private static final String BY_BROKER_USER_ID = BASE_QUERY + "WHERE e.realmId = :realmId && e.brokerUserId = :brokerUserId ORDER BY e.userSessionId";
+    private static final String BY_REALM = BASE_QUERY + "WHERE e.realmId = :realmId ORDER BY e.userSessionId";
 
     /**
-     * Returns a projection with the user session, and the version of all user sessions belonging to the broker session
-     * ID.
+     * Returns all user sessions belonging to the broker session ID.
      */
     public static Query<RemoteUserSessionEntity> searchByBrokerSessionId(RemoteCache<String, RemoteUserSessionEntity> cache, String realmId, String brokerSessionId) {
         return cache.<RemoteUserSessionEntity>query(BY_BROKER_SESSION_ID)
@@ -48,7 +48,7 @@ public final class UserSessionQueries {
     }
 
     /**
-     * Returns a projection with the user session, and the version of all user sessions belonging to the user ID.
+     * Returns all user sessions belonging to the user ID.
      */
     public static Query<RemoteUserSessionEntity> searchByUserId(RemoteCache<String, RemoteUserSessionEntity> cache, String realmId, String userId) {
         return cache.<RemoteUserSessionEntity>query(BY_USER_ID)
@@ -57,12 +57,19 @@ public final class UserSessionQueries {
     }
 
     /**
-     * Returns a projection with the user session, and the version of all user sessions belonging to the broker user
-     * ID.
+     * Returns all user sessions belonging to the broker user ID.
      */
     public static Query<RemoteUserSessionEntity> searchByBrokerUserId(RemoteCache<String, RemoteUserSessionEntity> cache, String realmId, String brokerUserId) {
         return cache.<RemoteUserSessionEntity>query(BY_BROKER_USER_ID)
                 .setParameter("realmId", realmId)
                 .setParameter("brokerUserId", brokerUserId);
+    }
+
+    /**
+     * Returns all the user sessions belonging to the Realm.
+     */
+    public static Query<RemoteUserSessionEntity> searchByRealm(RemoteCache<String, RemoteUserSessionEntity> cache, String realmId) {
+        return cache.<RemoteUserSessionEntity>query(BY_REALM)
+                .setParameter("realmId", realmId);
     }
 }
